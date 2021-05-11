6b8fa13319ef06d7ace4f5f4082042762cc62c09
Danke1732 <s0lfa1732@gmail.com>
2021-05-11 19:49:58 +0900

    広告アップロード処理のController実装

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AdsTopCreateRequest;
+use App\Http\Requests\AdsBottomCreateRequest;
+use App\Models\Ad;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
@@ -40,4 +44,110 @@ class AdminController extends Controller
         return redirect()->route('admin.showLogin');
     }
 
+    /**
+     * 広告一覧表示
+     * @return View
+     */
+    public function ads_list()
+    {
+        $ads_top_lists = Ad::where('location', 0)->get();
+        $ads_bottom_lists = Ad::where('location', 1)->get();
+        return view("admin.ads_list", ["ads_top_lists" => $ads_top_lists, "ads_bottom_lists" => $ads_bottom_lists]);
+    }
+
+    /**
+     * 広告（上）のアップロード処理
+     * @param App\Http\Requests\AdsTopCreateRequest $request
+     */
+    public function upload_top(AdsTopCreateRequest $request)
+    {
+        // 送られてきたデータを取得
+        $inputs = $request->all();
+        // 送られてきた画像情報を取得(変数へ代入)
+        $upload_image = $request->input('image1');
+        // Adインスタンスの作成
+        $ad = new Ad();
+       
+        // 画像が存在すれば条件分岐
+        if ($upload_image) {
+            // アップロードされた画像を保存する
+            // 画像のファイルの記述情報を$imgへ代入
+            $img = file_get_contents($upload_image);
+            // Adテーブルのidカラムの最大値を取得
+            $maxId = Ad::max('id');
+            // idの+1をファイル名にする
+            $ads_num = $maxId + 1;
+            $path = "ads_top-$ads_num";
+
+            // Storage/public内に画像ファイルを保存
+            Storage::disk('public')->put($path, $img);
+
+            if ($path) {
+                \DB::beginTransaction();
+                try {
+                    $ad->fill([
+                        'file_name' => $upload_image,
+                        'file_path' => Storage::url($path),
+                        'link' => $inputs['ad-link1'],
+                        'location' => $inputs['location'],
+                    ]);
+                    $ad->save();
+                    \DB::commit();
+                } catch(\Throwable $e) {
+                    \DB::rollback();
+                    return redirect()->route('admin.home');
+                }
+            }
+        } 
+
+        return redirect()->route('admin.home');
+    }
+
+    /**
+     * 広告（下）のアップロード処理
+     * @param App\Http\Requests\AdsBottomCreateRequest $request
+     */
+    public function upload_bottom(AdsBottomCreateRequest $request)
+    {
+        // 送られてきたデータを取得
+        $inputs = $request->all();
+        // 送られてきた画像情報を取得(変数へ代入)
+        $upload_image = $request->input('image2');
+        // Adインスタンスの作成
+        $ad = new Ad();
+       
+        // 画像が存在すれば条件分岐
+        if ($upload_image) {
+            // アップロードされた画像を保存する
+            // 画像のファイルの記述情報を$imgへ代入
+            $img = file_get_contents($upload_image);
+            // Adテーブルのidカラムの最大値を取得
+            $maxId = Ad::max('id');
+            // idの+1をファイル名にする
+            $ads_num = $maxId + 1;
+            $path = "ads_bottom-$ads_num";
+
+            // Storage/public内に画像ファイルを保存
+            Storage::disk('public')->put($path, $img);
+
+            if ($path) {
+                \DB::beginTransaction();
+                try {
+                    $ad->fill([
+                        'file_name' => $upload_image,
+                        'file_path' => Storage::url($path),
+                        'link' => $inputs['ad-link2'],
+                        'location' => $inputs['location'],
+                    ]);
+                    $ad->save();
+                    \DB::commit();
+                } catch(\Throwable $e) {
+                    \DB::rollback();
+                    return redirect()->route('admin.home');
+                }
+            }
+        } 
+
+        return redirect()->route('admin.home');
+    }
 }
